979c7927a77414abb6dfbbe366d7640c534f51ed
WebRazr <razrsvob@gmail.com>
2020-08-31 10:18:38 +0300

    add prettier.

--- components/ShowAllTasks.tsx
+++ components/ShowAllTasks.tsx
@@ -1,22 +1,22 @@
 import React, { useState, useEffect } from "react";
 import ListPosts from "./ListPosts";
-import {MyPost} from "../interfaces/post";
-import styled from 'styled-components';
+import { MyPost } from "../interfaces/post";
+import styled from "styled-components";
 interface PostPageProps {
-  post: MyPost[]
+  post: MyPost[];
 }
 const GridPost = styled.div`
-margin: auto;
+  margin: auto;
   display: grid;
   width: 90%;
   grid-template-columns: 1fr 1fr 1fr;
   grid-gap: 2vw;
-`
-export default function ShowAllTasks({post}: PostPageProps) {
+`;
+export default function ShowAllTasks({ post }: PostPageProps) {
   let [postsD, setPostsD] = useState(post);
 
-    const list = postsD.map((el) => {
-      return <ListPosts key={el.id} dataPosts={el} />;
-    });
-    return <GridPost>{list}</GridPost>
+  const list = postsD.map((el) => {
+    return <ListPosts key={el.id} dataPosts={el} />;
+  });
+  return <GridPost>{list}</GridPost>;
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,30 +2,30 @@ import MainLayout from "../components/MainLayout";
 import ShowAllTasks from "../components/ShowAllTasks";
 import HeaderLayout from "../components/HeadLayout";
 import axios from "axios";
-import {MyPost} from "../interfaces/post";
-import styled from 'styled-components';
+import { MyPost } from "../interfaces/post";
+import styled from "styled-components";
 interface PostPageProps {
-    posts: MyPost[]
+  posts: MyPost[];
 }
 const TitlePost = styled.h1`
-   margin-left: 100px;
-`
-export default function Index({posts: serverPost}:PostPageProps) {
-    console.log(serverPost)
+  margin-left: 100px;
+`;
+export default function Index({ posts: serverPost }: PostPageProps) {
+  console.log(serverPost);
   return (
     <>
-     <HeaderLayout/>
+      <HeaderLayout />
       <MainLayout>
         <TitlePost>POST</TitlePost>
-        <ShowAllTasks post={serverPost}/>
+        <ShowAllTasks post={serverPost} />
       </MainLayout>
     </>
   );
 }
 Index.getInitialProps = async () => {
-    let data = await axios.get(`https://simple-blog-api.crew.red/posts`);
-    let dataRes: MyPost[] = await data.data;
-    return {
-        posts: dataRes
-    }
-}
+  let data = await axios.get(`https://simple-blog-api.crew.red/posts`);
+  let dataRes: MyPost[] = await data.data;
+  return {
+    posts: dataRes,
+  };
+};
